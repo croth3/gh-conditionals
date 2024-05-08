be099a504143d9d3147917da59f8dcd1aff4cb1e
Roth <croth3@luc.edu>
2024-05-08 11:31:51 -0500

    broke test2

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -19,6 +19,6 @@ describe('MainContent', () => {
 
     const button = screen.getByRole('button');
     await userEvent.click(button);
-    expect(screen.getByTestId('help-areaxx')).toBeInTheDocument();
+    expect(screen.getByTestId('help-areaxxx')).toBeInTheDocument();
   });
 });
